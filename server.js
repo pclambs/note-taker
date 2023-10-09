52af9b550d3e7ee72d13ec2cd3c4a7f33bc7550a
deepwubs <pchucklambs@gmail.com>
2023-10-09 18:37:59 -0500

    added server.js and empty routers

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const viewRoutes = require("./controllers/viewRoutes")
+const apiRoutes = require("./controllers/apiRoutes")
+
+const app = express();
+const PORT = process.env.PORT || 3000
+
+// Middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+})
